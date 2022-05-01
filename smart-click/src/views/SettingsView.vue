5ce584fb3455f0472dbae98f462e914fa8ddae69
lucasfleischer07 <lfleischer@itba.edu.ar>
2022-05-01 17:05:41 -0300

    Adding menu to SettingsView

--- smart-click/src/views/SettingsView.vue
+++ smart-click/src/views/SettingsView.vue
@@ -1,78 +1,151 @@
 <template>
   <div>
-<!--    <v-list  class="config-options">-->
-<!--      <v-list-group-->
-<!--              :value="true"-->
-<!--            >-->
-<!--          <v-icon>settings</v-icon>-->
-<!--          <span>General</span>-->
-<!--          <div>-->
-<!--            <h5>Terminos y Condiciones</h5>-->
-<!--            <h5>Modo oscuro</h5>-->
-<!--            <h5>Config 3</h5>-->
-<!--          </div>-->
+<!--    class="mx-auto" -> meter este atributo y va a quedar centrado-->
+    <v-card width="500" color="secondary">
+      <v-list>
+        <v-list-group prepend-icon="settings">
+          <template v-slot:activator>
+            <v-list-item-title>General</v-list-item-title>
+          </template>
 
-<!--          <v-icon>security</v-icon>-->
-<!--          <span>Ayuda</span>-->
-<!--          <div>-->
-<!--            <h5>Agregar Dispositivos</h5>-->
-<!--            <h5>Agregar Casas y Cuartos</h5>-->
-<!--            <h5>Crear una Rutina</h5>-->
-<!--          </div>-->
+          <v-list-group no-action sub-group>
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <!--              Ver si quieren sacar esto o no (el icono)      -->
+                  <v-icon>receipt_long</v-icon>
+                  Terminos y Condiciones
+                </v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item> Insertar Terminos y condiciones aca</v-list-item>
+          </v-list-group>
 
-<!--          <v-icon>notifications_active</v-icon>-->
-<!--          <span>Notificaciones</span>-->
-<!--          <div>-->
-<!--            <h5>Activar </h5>-->
-<!--            <h5>Config 2</h5>-->
-<!--            <h5>Config 3</h5>-->
-<!--          </div>-->
-<!--        </v-list-group>-->
-<!--    </v-list>-->
+          <v-list-item>
+            <v-switch v-model="darkMode" label="Modo Oscuro"></v-switch>
+<!--            <v-list-item-icon><v-icon>dark_mode</v-icon></v-list-item-icon>-->
+          </v-list-item>
 
-    <v-list>
-      <v-list-group
-          :value="true"
-          prepend-icon="mdi-account-circle">
-        <template v-slot:activator>
-          <v-list-item-title>General</v-list-item-title>
-        </template>
+          <v-list-group no-action sub-group>
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <!--              Ver si quieren sacar esto o no (el icono)      -->
+                  <v-icon>dark_mode</v-icon>
+                  Config 3</v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item> Ver que poner o lo sacamos</v-list-item>
+          </v-list-group>
+        </v-list-group>
 
-        <v-list-item
-            v-for="(title, i) in general"
-            :key="i"
-            link>
-          <v-list-item-title v-text="title"></v-list-item-title>
+        <!------------------------------------------------------------------------------------------------------------->
 
-          <v-list-item-icon>
-            <v-icon v-text="icon"></v-icon>
-          </v-list-item-icon>
-        </v-list-item>
-      </v-list-group>
-
-      <v-list-group
-          :value="true"
-          prepend-icon="mdi-account-circle">
-
-        <template v-slot:activator>
-          <v-list-item-content>
+        <v-list-group prepend-icon="lock">
+          <template v-slot:activator>
             <v-list-item-title>Privacidad</v-list-item-title>
-          </v-list-item-content>
-        </template>
+          </template>
 
-        <v-list-item
-            v-for="([title, icon], i) in cruds"
-            :key="i"
-            link
-        >
-          <v-list-item-title v-text="title"></v-list-item-title>
+          <v-list-group no-action sub-group>
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <!--              Ver si quieren sacar esto o no (el icono)      -->
+                  <v-icon>receipt_long</v-icon>
+                  VER QUE METER ACA
+                </v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item>VER QUE METER ACA</v-list-item>
+          </v-list-group>
 
-          <v-list-item-icon>
-            <v-icon v-text="icon"></v-icon>
-          </v-list-item-icon>
-        </v-list-item>
-      </v-list-group>
-    </v-list>
+          <v-list-group no-action sub-group>
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <!--              Ver si quieren sacar esto o no (el icono)      -->
+                  <v-icon>dark_mode</v-icon>
+                  VER QUE METER ACA</v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item> Ver que poner o lo sacamos</v-list-item>
+          </v-list-group>
+
+          <v-list-group no-action sub-group>
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <!--              Ver si quieren sacar esto o no (el icono)      -->
+                  <v-icon>dark_mode</v-icon>
+                  VER QUE METER ACA</v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item> Ver que poner o lo sacamos</v-list-item>
+          </v-list-group>
+        </v-list-group>
+
+<!------------------------------------------------------------------------------------------------------------->
+
+        <v-list-group prepend-icon="help">
+          <template v-slot:activator>
+            <v-list-item-title>Ayuda</v-list-item-title>
+          </template>
+
+          <v-list-group no-action sub-group>
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <!--              Ver si quieren sacar esto o no (el icono)      -->
+                  <v-icon>home</v-icon>
+                  Agregar casa
+                </v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item>VER QUE METER ACA</v-list-item>
+          </v-list-group>
+
+          <v-list-group no-action sub-group>
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <!--              Ver si quieren sacar esto o no (el icono)      -->
+                  <v-icon>bed</v-icon>
+                  Agregar cuarto</v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item> Ver que poner o lo sacamos</v-list-item>
+          </v-list-group>
+
+          <v-list-group no-action sub-group>
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <!--              Ver si quieren sacar esto o no (el icono)      -->
+                  <v-icon>devices</v-icon>
+                  Agregar dispositivo</v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item> Ver que poner o lo sacamos</v-list-item>
+          </v-list-group>
+
+          <v-list-group no-action sub-group>
+            <template v-slot:activator>
+              <v-list-item-content>
+                <v-list-item-title>
+                  <!--              Ver si quieren sacar esto o no (el icono)      -->
+                  <v-icon>calendar_month</v-icon>
+                  Crear una rutina</v-list-item-title>
+              </v-list-item-content>
+            </template>
+            <v-list-item> Ver que poner o lo sacamos</v-list-item>
+          </v-list-group>
+        </v-list-group>
+
+        <!------------------------------------------------------------------------------------------------------------->
+
+
+      </v-list>
+    </v-card>
   </div>
 </template>
 
@@ -85,6 +158,7 @@
           general: [['Terminos y Condiciones',2],['Modo Oscuro',3],['Config 3',4]],
           security: [['Terminos y Condiciones',5],['Modo Oscuro',6],['Config 3',7]],
           notificaciones: [['Terminos y Condiciones',8],['Modo Oscuro',9],['Config 3',2]],
+        darkMode: false
       }
     }
 }
@@ -93,7 +167,4 @@
 </script>
 
 <style scoped>
-    .config-options{
-        text-align: left;
-    }
 </style>
