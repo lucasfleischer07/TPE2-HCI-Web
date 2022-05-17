8459211d38efe54a38c0108ad2e42786f2cbb7f3
lucasfleischer07 <lfleischer@itba.edu.ar>
2022-05-17 13:23:11 -0300

    Style changes oven

--- smart-click/src/components/Horno.vue
+++ smart-click/src/components/Horno.vue
@@ -30,27 +30,27 @@
       </v-row>
       <p style="padding-top: 30px">Fuente calor</p>
       <v-row class="action-row action_btn">
-        <v-btn-toggle mandatory v-model="fuenteCalor" @click="setHeatFunction">
-          <v-btn width="100px">ABAJO</v-btn>
-          <v-btn width="100px">NORMAL</v-btn>
-          <v-btn width="100px">ARRIBA</v-btn>
+        <v-btn-toggle mandatory v-model="fuenteCalor">
+          <v-btn @click="setHeatFunction" width="100px">ABAJO</v-btn>
+          <v-btn @click="setHeatFunction" width="100px">NORMAL</v-btn>
+          <v-btn @click="setHeatFunction" width="100px">ARRIBA</v-btn>
         </v-btn-toggle>
       </v-row>
       <p style="padding-top: 30px">Grill</p>
       <v-row class="action-row action_btn">
-        <v-btn-toggle mandatory v-model="grillMode" class="grill-buttons" @click="setGrillFunction">
-          <v-btn width="100px"><v-icon>power_off</v-icon></v-btn>
-          <v-btn width="100px"><v-icon>energy_savings_leaf</v-icon></v-btn>
-          <v-btn width="100px"><v-icon>bolt</v-icon></v-btn>
+        <v-btn-toggle mandatory v-model="grillMode" class="grill-buttons" >
+          <v-btn @click="setGrillFunction" width="100px"><v-icon>power_off</v-icon></v-btn>
+          <v-btn @click="setGrillFunction" width="100px"><v-icon>energy_savings_leaf</v-icon></v-btn>
+          <v-btn @click="setGrillFunction" width="100px"><v-icon>bolt</v-icon></v-btn>
         </v-btn-toggle>
       </v-row>
 
       <p style="padding-top: 30px">Convección</p>
       <v-row class="action-row action_btn">
-        <v-btn-toggle mandatory v-model="conveccionMode" @click="setConvectionFunction">
-          <v-btn width="100px"><v-icon>power_off</v-icon></v-btn>
-          <v-btn width="100px"><v-icon>energy_savings_leaf</v-icon></v-btn>
-          <v-btn width="100px"><v-icon>bolt</v-icon></v-btn>
+        <v-btn-toggle mandatory v-model="conveccionMode">
+          <v-btn @click="setConvectionFunction" width="100px"><v-icon>power_off</v-icon></v-btn>
+          <v-btn @click="setConvectionFunction" width="100px"><v-icon>energy_savings_leaf</v-icon></v-btn>
+          <v-btn @click="setConvectionFunction" width="100px"><v-icon>bolt</v-icon></v-btn>
         </v-btn-toggle>
       </v-row>
     </v-card>
@@ -68,7 +68,6 @@ export default {
 
   props: {
     deviceEntity: {},
-
   },
 
 
@@ -101,7 +100,15 @@ export default {
     },
 
     async setHeatFunction() {
-      let params = [this.deviceEntity.id, "setHeat", [this.fuenteCalor]]
+      let heat;
+      if(this.fuenteCalor === 0) {
+        heat = "bottom";
+      } else if(this.fuenteCalor === 1) {
+        heat = "conventional";
+      } else if(this.fuenteCalor === 2) {
+        heat = "top";
+      }
+      let params = [this.deviceEntity.id, "setHeat", [heat]]
       try {
         await this.$execute(params)
       } catch (e) {
@@ -110,7 +117,15 @@ export default {
     },
 
     async setGrillFunction() {
-      let params = [this.deviceEntity.id, "setGrill", [this.grillMode]]
+      let grill;
+      if(this.grillMode === 0) {
+        grill = "off";
+      } else if(this.grillMode === 1) {
+        grill = "eco";
+      } else if(this.grillMode === 2) {
+        grill = "large";
+      }
+      let params = [this.deviceEntity.id, "setGrill", [grill]]
       try {
         await this.$execute(params)
       } catch (e) {
@@ -119,7 +134,15 @@ export default {
     },
 
     async setConvectionFunction() {
-      let params = [this.deviceEntity.id, "setGrill", [this.conveccionMode]]
+      let convection;
+      if(this.conveccionMode == 0) {
+        convection = "off";
+      } else if(this.conveccionMode == 1) {
+        convection = "eco";
+      } else if(this.conveccionMode == 2) {
+        convection = "normal";
+      }
+      let params = [this.deviceEntity.id, "setConvection", [convection]]
       try {
         await this.$execute(params)
       } catch (e) {
