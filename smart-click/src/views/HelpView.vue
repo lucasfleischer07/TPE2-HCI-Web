54764bfac87d47ad3c9ebf3e45dcefdb10626235
Malena Banfi <mabanfi@itba.edu.ar>
2022-05-18 12:52:40 -0300

    Help changes

--- smart-click/src/views/HelpView.vue
+++ smart-click/src/views/HelpView.vue
@@ -89,7 +89,7 @@
                 </v-list-item-title>
               </v-list-item-content>
             </template>
-            <v-list-item class="text-alignment"> Para poder agregar una casa, en la barra superior, tocar el selector de casa. Debajo de las otras casas (si las hay) encontrará un botón con la opción "agregar casa". También puede agregar una casa desde la página principal presionando el botón de "agregar casa".
+            <v-list-item class="text-alignment"> Para poder agregar una casa, en la barra superior, presionar el selector de casa. Debajo de las otras casas (si las hay) encontrará un botón con la opción "agregar casa". También puede agregar una casa desde la página principal presionando el botón de "agregar casa".
             </v-list-item>
           </v-list-group>
 
@@ -102,8 +102,7 @@
                 </v-list-item-title>
               </v-list-item-content>
             </template>
-            <v-list-item class="text-alignment"> ESCRIBIR ACA
-            </v-list-item>
+            <v-list-item class="text-alignment"> Para poder eliminar una casa, en la barra superior, presionar el selector de casa. Seleccione la casa que desea eliminar. Luego aprete el botón de dispositivos en la barra de navegación. En dicha vista verá el nombre de su casa con un tacho de basura rojo a la derecha. Apretelo y la casa se eliminará. </v-list-item>
           </v-list-group>
 
         </v-list-group>
@@ -124,7 +123,7 @@
                 </v-list-item-title>
               </v-list-item-content>
             </template>
-            <v-list-item class="text-alignment">Para poder agregar una habitación en una casa, primero se debe agregar o seleccionar una casa. Si necesita ayuda para realizar este paso, vea el ítem anterior de ayuda. Una vez que haya una casa seleccionada, ir al apartado dispositivos y tocar el botón "+" a la derecha de las habitaciones. </v-list-item>
+            <v-list-item class="text-alignment">Para poder agregar una habitación en una casa, primero se debe agregar o seleccionar una casa. Si necesita ayuda para realizar este paso, vea el ítem anterior de ayuda. Una vez que haya una casa seleccionada, ir al apartado dispositivos y tocar el botón "+" a la derecha de las habitaciones. Apretelo y la habitación se eliminará.</v-list-item>
           </v-list-group>
 
           <v-list-group no-action sub-group>
@@ -135,7 +134,7 @@
                   Eliminar</v-list-item-title>
               </v-list-item-content>
             </template>
-            <v-list-item class="text-alignment">VERE QUE ESCRIBIR </v-list-item>
+            <v-list-item class="text-alignment"> Para poder eliminar una habitación de una casa, primero situese en dicha casa. Seleccione el boton de dispositivos en la barra de navegación. Dentro encontrará su habitacion y verá un tacho de basura rojo a la derecha del nombre de la habitación. Apretelo y la habitación se eliminará. </v-list-item>
           </v-list-group>
         </v-list-group>
 
@@ -154,7 +153,7 @@
                 </v-list-item-title>
               </v-list-item-content>
             </template>
-            <v-list-item class="text-alignment"> Para agregar un dispositivo, debe tener una casa y un cuarto agregados. Teniendo esto, ir al apartado dispositivos, elegir casa(si necesario) y una habitación. Luego presionar el botón "agregar dispositivo", elegir el tipo de dispositivo y luego presionar "confirmar".
+            <v-list-item class="text-alignment"> Para agregar un dispositivo, debe tener una casa y un habitación agregados. Teniendo esto, ir al apartado dispositivos, elegir casa(si necesario) y una habitación. Luego presionar el botón "agregar dispositivo", elegir el tipo de dispositivo y luego presionar "confirmar".
             </v-list-item>
           </v-list-group>
 
@@ -167,8 +166,7 @@
                 </v-list-item-title>
               </v-list-item-content>
             </template>
-            <v-list-item class="text-alignment"> ESCRIBIR ACA
-            </v-list-item>
+            <v-list-item class="text-alignment"> Para poder eliminar un dispositivo, debe tener una casa y un cuarto seleccionados. Ingrese en la habitación donde se encuentra su dispositivo. En dicho dispositivo encontrará un tacho de basura rojo. Apretelo y el dispositivo se eliminará.</v-list-item>
           </v-list-group>
         </v-list-group>
 
@@ -187,7 +185,7 @@
                 </v-list-item-title>
               </v-list-item-content>
             </template>
-            <v-list-item class="text-alignment"> Para poder agregar una rutina, debe tener al menos un dispositivo agregado en su casa. Debe ir al apartado de rutinas, presionar "+". Luego debe seleccionar los dispositivos que desea vincular en la rutina, determinar la acción a realizar y su horario, agregar el dispositivos a la rutina y repetir para todos los dispositivos. Una vez que haya agregado todos los dispositivos necesarios, presionar "crear rutina" y habrá finalizado la creación.
+            <v-list-item class="text-alignment"> Para poder agregar una rutina, debe tener al menos un dispositivo agregado en su habitación. Debe ir al apartado de rutinas, presionar "+". Luego debe seleccionar los dispositivos que desea vincular en la rutina, determinar la acción a realizar y su horario, agregar el dispositivos a la rutina y repetir para todos los dispositivos. Una vez que haya agregado todos los dispositivos necesarios, presionar "crear rutina" y habrá finalizado la creación.
             </v-list-item>
           </v-list-group>
 
@@ -200,7 +198,7 @@
                 </v-list-item-title>
               </v-list-item-content>
             </template>
-            <v-list-item class="text-alignment"> ESCRIBIR ACA
+            <v-list-item class="text-alignment"> Para poder eliminar una rutina, debe tener una casa seleccionada. Debe ir al apartado de rutinas. A la derecha del nombre de su rutina encontrará un tacho de basura rojo. Apretelo y la rutina se eliminará.
             </v-list-item>
           </v-list-group>
         </v-list-group>
